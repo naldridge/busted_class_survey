43c0baa462c1eb446a728b5ac53ccf9077bc8dbf
Nick Aldridge <naldridge1360@gmail.com>
2021-06-24 10:25:34 -0500

    solved

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,10 +1,10 @@
 CREATE TABLE rankings (
     id serial PRIMARY KEY,
-    ranking_title text NOT NULL
+    ranking_title TEXT NOT NULL
 );
 
 CREATE TABLE topics (
     id serial PRIMARY KEY,
-    topic_name text NOT NULL,
-    topic_score INTEGER REFERENCES rankings (id)
+    topic_name TEXT NOT NULL,
+    topic_score INTEGER REFERENCES rankings(id)
 );
